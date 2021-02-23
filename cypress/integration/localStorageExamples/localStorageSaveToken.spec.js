3bc2bf7cb986afacd39471c97c06e3bc40e588e8
Arthur Mazur <46711836+ArthurKazusMazur@users.noreply.github.com>
2021-02-23 11:03:03 +0300

    changed file structure

--- cypress/integration/localStorageExamples/localStorageSaveToken.spec.js
+++ cypress/integration/localStorageExamples/localStorageSaveToken.spec.js
@@ -1,22 +1,6 @@
-import * as inputData from "../fixtures/nero_login.json";
-import { settingsButton } from "../page-objects/nero_generalPage";
-import LoginPage from "../page-objects/nero_loginPage";
-
-describe("User can start session from desired page onle after login", () => {
-  it("User logs in via UI and goes to system settings", () => {
-    cy.visit(inputData.url);
-    LoginPage.ordinaryUserLogin(inputData.login, inputData.password);
-    cy.url().should("include", "general");
-    cy.get(settingsButton).click();
-    cy.url().should("include", "settings");
-  });
-
-  // will fail
-  it("User is trying to start session from Settings page", () => {
-    cy.visit(inputData.url + "/settings");
-    cy.url().should("include", "settings");
-  });
-});
+import * as inputData from "../../fixtures/nero_login.json";
+import { settingsButton } from "../../page-objects/nero_generalPage";
+import LoginPage from "../../page-objects/nero_loginPage";
 
 describe("Avoid multy login via UI by saving token to Local Storage", () => {
   beforeEach("Login via api", () => {
