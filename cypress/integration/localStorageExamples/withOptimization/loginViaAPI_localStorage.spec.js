8fde250b6c5dd0b4325cb70f60a3e26ea5d16b44
Arthur Mazur <46711836+ArthurKazusMazur@users.noreply.github.com>
2021-03-09 15:17:06 +0300

    Local Storage tricks: restructuring project

---
+++ cypress/integration/localStorageExamples/withOptimization/loginViaAPI_localStorage.spec.js
@@ -0,0 +1,17 @@
+import * as inputData from "../../../fixtures/nero_login.json";
+
+describe("Avoid multy login via UI by saving token to Local Storage", () => {
+  beforeEach("Login via api", () => {
+    cy.loginWithToken(inputData.login, inputData.password);
+  });
+
+  it("User is starting session from General page", () => {
+    cy.visit(inputData.url + "/general");
+    cy.url().should("include", "general");
+  });
+
+  it("User is starting session from Settings page", () => {
+    cy.visit(inputData.url + "/settings");
+    cy.url().should("include", "settings");
+  });
+});
