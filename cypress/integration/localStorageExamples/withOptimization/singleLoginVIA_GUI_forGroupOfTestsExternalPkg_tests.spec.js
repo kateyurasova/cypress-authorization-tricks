607f463a5f8b9a98eb9c1d76fb6d39b2e8e9a918
Arthur Mazur <46711836+ArthurKazusMazur@users.noreply.github.com>
2021-03-17 16:43:25 +0300

    avoid dependant tests

--- cypress/integration/localStorageExamples/withOptimization/singleLoginVIA_GUI_forGroupOfTestsExternalPkg_tests.spec.js
+++ cypress/integration/localStorageExamples/withOptimization/singleLoginVIA_GUI_forGroupOfTestsExternalPkg_tests.spec.js
@@ -10,52 +10,54 @@ allows to simulate that localStorage is disabled in the browser.
 import * as inputData from "../../../fixtures/nero_login.json";
 import {
   goToSettings,
+  showWhatisFound,
   canvasNotifications,
   goToGeneral,
 } from "../../../page-objects/nero_generalPage";
 import LoginPage from "../../../page-objects/nero_loginPage";
 
-describe("Using external package", () => {
-  describe("Login via GUI and SAVE LOCAL STORAGE once", () => {
-    it("[ПОКАЗАТЬ ПОИСК] is displayed on the General page", () => {
-      cy.visit(inputData.url);
-      LoginPage.ordinaryUserLogin(inputData.login, inputData.password);
-      cy.url().should("include", "general");
-      cy.get(goToSettings).click();
-      cy.url().should("include", "settings");
-      cy.saveLocalStorage();
-    });
+// Using external package
+before("Login via GUI and SAVE LOCAL STORAGE once", () => {
+  cy.visit(inputData.url);
+  LoginPage.ordinaryUserLogin(inputData.login, inputData.password);
+  cy.url().should("include", "general");
+  cy.saveLocalStorage();
+});
+
+describe("Starting tests with restoring LOCAL STORAGE before each", () => {
+  beforeEach("Restoring Local Storage data before every test", () => {
+    cy.restoreLocalStorage();
   });
 
-  describe("Starting tests with restoring LOCAL STORAGE before each", () => {
-    beforeEach("Restoring Local Storage data before every test", () => {
-      cy.restoreLocalStorage();
-    });
+  it("[ПОКАЗАТЬ ПОИСК] is displayed on the General page", () => {
+    cy.visit(inputData.url);
+    cy.url().should("include", "general");
+    cy.get(showWhatisFound).should("be.visible");
+  });
 
-    it.only(`'Оповещение' is on the page Notifications`, () => {
-      cy.visit(inputData.url + "/notifications");
-      cy.url().should("include", "/notifications");
-      cy.get(canvasNotifications).should("contain", "Оповещения");
-    });
+  it(`'Оповещение' is on the page Notifications`, () => {
+    cy.visit(inputData.url + "/notifications");
+    cy.url().should("include", "/notifications");
+    cy.get(canvasNotifications).should("contain", "Оповещения");
+  });
 
-    it(`Notifications. User goes back to General page`, () => {
-      cy.visit(inputData.url + "/notifications");
-      cy.url().should("include", "/notifications");
-      cy.contains(goToGeneral).click();
-      cy.url().should("include", "/general");
-    });
+  it(`Notifications. User goes back to General page`, () => {
+    cy.visit(inputData.url + "/notifications");
+    cy.url().should("include", "/notifications");
+    cy.contains(goToGeneral).click();
+    cy.url().should("include", "/general");
+  });
 
-    it(`'Настройки' is on the page Settings`, () => {
-      cy.visit(inputData.url + "/settings");
-      cy.url().should("include", "/settings");
-      cy.get(canvasNotifications).should("contain", "Настройки");
-    });
+  it(`'Настройки' is on the page Settings`, () => {
+    cy.visit(inputData.url + "/settings");
+    cy.url().should("include", "/settings");
+    cy.get(canvasNotifications).should("contain", "Настройки");
+  });
 
-    it("Settings. User goes back to General page", () => {
-      cy.visit(inputData.url + "/settings");
-      cy.url().should("include", "/settings");
-      cy.contains(goToGeneral).click();
-      cy.url().should("include", "/general");
-    });
+  it("Settings. User goes back to General page", () => {
+    cy.visit(inputData.url + "/settings");
+    cy.url().should("include", "/settings");
+    cy.contains(goToGeneral).click();
+    cy.url().should("include", "/general");
   });
 });
